900af5c08249b7a3bde845850d76bdbd275cb7d1
Ajeet Jha <ajeetjha1@gmail.com>
2015-01-30 14:23:23 +0530

    Cleanup functionality and doc added.
    Signed-off-by: Ajeet Jha <ajeetjha1@gmail.com>

--- bapiCli.py
+++ bapiCli.py
@@ -1,8 +1,4 @@
-'''
-collect backup list
-fetch backup list
-cleanup backup list
-'''
+
 import os
 import io
 import sys
@@ -10,26 +6,84 @@ import re
 import time
 import subprocess
 import hashlib
+import shutil
+
 
 def getVolInfo (volName):
+    '''
+    Extracts list of tuple (brick_hostname, brick_path)
+    from "gluster volume info" command.
+
+    @volName: Volume name for which the info is extracted.
+
+    Returns:
+    Success: List of Tuple (brick_hostname, brick_path)
+    Failure: None
+
+    '''
     gvi = subprocess.Popen(["gluster", "volume", "info", volName],
                            stdout=subprocess.PIPE)
     info, err = gvi.communicate()
-    
-    print info
+
+    # ajha: handle error , return None
+
     brickDetails = []
-    
+
     for line in info.split("\n"):
         brickInfo = re.search(r'^Brick[0-9]+',line)
         if brickInfo:
-            print line
             details = line.split(":")
             brickDetails.append ((details[1].strip(), details[2].strip()))
             print details[1], details[2]
-    
+
     return brickDetails
 
+def clean (dirPath):
+    '''
+    Recursively unlinks everything *inside* @dirPath, but not the
+    @dirPath itself.
+
+    @dirPath: Directory to clean.
+
+    Returns: None
+    '''
+    for root, dirs, files in os.walk (dirPath):
+        for f in files:
+            os.remove (os.path.join (root, f))
+        for d in dirs:
+            shutil.rmtree (os.path.join (root, d))
+    pass
+
+def sortUnique(fileName):
+    '''
+    Sorts the fileName with unique entries.
+
+    @fileName: File to sort uniquely.
+
+    Returns: None
+
+    '''
+    p = subprocess.Popen(["sort", "-u", "-o",fileName, fileName], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
 def collect (host, brickPath, scratchDir, start, end):
+    '''
+    Envoking get-history script at @host and collecting backup details
+    at "@scratchDir/backup".
+
+    @host       : Hostname at which the script should be envoked.
+    @brickPath  : Absolute path of the brick location
+    @scratchDir : Global  scratch directory used by backup API
+                  Contains hashed directory containing backup info
+                  of each brick, history api's additional directories,
+                  "backup" containing backup details,
+                  "collection" containing remotely fetched backup details.
+    @start      : Start time from which backup details is required.
+    @end        : End time till which backup details is required.
+
+    Returns: None
+
+    '''
     HOST = host
     COMMAND = "python /backup/src/bapi/parseCl.py " + \
                 brickPath +" " + scratchDir + " " + \
@@ -40,7 +94,7 @@ def collect (host, brickPath, scratchDir, start, end):
                            shell=False,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    
+
     result, err = ssh.communicate()
     '''
     if result == []:
@@ -50,11 +104,32 @@ def collect (host, brickPath, scratchDir, start, end):
         print result
     '''
 
-def fetch(host, brick, scratchDir, count):
-    # args, brick_ip, scratch_dir
-    hashDir = str( hashlib.sha1(brick).hexdigest() )
+def fetch(host, brick, scratchDir, identifier):
+    '''
+    Fetches the backup detailes created by 'runCollect' from
+    @host and stores it at 'collection' directory under
+    '@scratchDir/collection'.
+
+    List of files from different remote nodes are prefixed with 'remote'.
+    Final list of files is named as 'backuplist.full'.
+
+    @host       : Hostname from which backup details are to be fetched.
+    @brick      : Absolute path of the brick location.
+    @scratchDir : Global  scratch directory used by backup API
+                  Contains hashed directory containing backup info
+                  of each brick, history api's additional directories,
+                  "backup" containing backup details,
+                  "collection" containing remotely fetched backup details.
+    @identifier : Counter to distinguish between backup details from name node.
+
+    Returns: None
+
+    '''
+
+    hashDir = str (hashlib.sha1(brick).hexdigest())
     srcPath = os.path.join (scratchDir, hashDir, "backup", "backup_list")
-    destFname = "remote_list" + re.sub(r'\.', "_", host) + "_" + str(count)
+
+    destFname = "remote_list" + re.sub(r'\.', "_", host) + "_" + str(identifier)
     destPath = os.path.join (scratchDir,"collection", destFname)
 
     HOST = host
@@ -63,32 +138,49 @@ def fetch(host, brick, scratchDir, count):
     out, err = scp.communicate()
 
     print out
-    # check is backup_list file is there or not
-    # if there get it
-    # collect at scratch_dir/backup/backup.ip.path
-    # accumulate all pathnames
-    # do sort -u
-    pass
 
-def cleanup():
-    # args: brick_ip, scratch_dir
-    # envoke recursive unlink on everythin inside scratch_dir
-    # always check if the patch is not (/)
-    # rmdir scratch_dir
-    pass
+def runCleanup(volName, scratchDir):
+    '''
+    Cleans the Global scratchDir at volume level for next backup run.
+
+    @volName    : volume name for which backupapi was envoked.
+    @scratchDir : scratchDir used by that volume.
+
+    Returns: None
+    '''
+    HOST = host
+    COMMAND = "python /backup/src/bapi/parseCl.py " + scratchDir
+
+    ssh = subprocess.Popen(["ssh", "%s" % HOST , COMMAND],
+                           shell=False,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE)
+
+    result, err = ssh.communicate()
 
 def runCollect (volName, scratchDir, start, end):
-    # file exists OSError
+    '''
+    Runs 'collect' on each distribute node of @volName.
+
+    Returns: None
+    '''
     volInfo = getVolInfo (volName)
     for host, brickPath in volInfo:
         collect (host, brickPath, scratchDir, start, end)
 	print "Done collecting at: ", host , brickPath
 
-    # create backuplist.full 
-    # readdir collectionDir, keep reading from all files and add to "backuplist.full"
 
+def runFetch (volName, scratchDir, outFile=None):
+    '''
+    Runs 'fetch' on each distribute node of @volName.
+    Get the volume level backup details in
+    '@scratchDir/collection/backupList.full'.
 
-def runFetch (volName, scratchDir):
+    If outFile is given, copies the backupList.full to outFile.
+
+    Returns: None
+    '''
+
     collectionDir = os.path.join (scratchDir, "collection")
     os.mkdir (collectionDir)
     volInfo = getVolInfo (volName)
@@ -108,3 +200,10 @@ def runFetch (volName, scratchDir):
                     with open(blistPath) as bfd:
                         for line in bfd:
                             fd.write(line)
+
+    if outFile is not None:
+        # copy backupList.full to outFile
+        # if outFile is not absolute path, get current working directory
+        # and create outfile, write to it.
+        pass
+
--- parseCl.py
+++ parseCl.py
@@ -13,52 +13,116 @@ import time
 import libgfchangelog
 import subprocess
 import hashlib
+import shutil
 
 cl = libgfchangelog.Changes()
 
 
 def gfidToPath (gPath, scratch_dir, brickPath):
+    '''
+    Creates new file at location @scratch_dir/backup/backup_list'
+    containing list of absolute paths wrt to each gfid in
+    '@scratch_dir/backup/gfid_list'.
+
+    @gPath  : Absolute path to 'gfid_list'.
+    @scratch_dir : Scratch directory for history API.
+                   Its @scratchDir/<brickPath-hash>.
+    @brickPath : Absolute path to brick location.
+
+    Returns: None
+
+    '''
     backupList = os.path.join(scratch_dir, "backup", "backup_list")
 
     with open(gPath) as gfd:
-        #print "reading gfids..."
+
         bfd = open(backupList,"a+")
         for gfid in gfd:
-            #print gfid
-            # find path
-            # p = subprocess.Popen(["ls", "-l", "/etc/resolv.conf"], stdout=subprocess.PIPE)
-            #output, err = p.communicate()
-            # find /export1/v1/b1/ -samefile /export1/v1/b1/.glusterfs/bc/ad/bcade943-9d21-47e1-8faa-7d3dfbdee100
-            backendPath = os.path.join(brickPath,".glusterfs", gfid[0:2], gfid[2:4],gfid[0:-1]) 
-            p = subprocess.Popen(["find", brickPath, "-samefile",backendPath],  stdout=subprocess.PIPE)
+            backendPath = os.path.join(brickPath,".glusterfs",\
+                          gfid[0:2], gfid[2:4],gfid[0:-1])
+
+            p = subprocess.Popen(["find", brickPath, "-samefile",\
+                                 backendPath],  stdout=subprocess.PIPE)
             out, err = p.communicate()
+
             paths = out.split()
+
             for path in paths:
                 if ".glusterfs" not in path:
-                    #print "me  ", path
-                    bfd.write (path + "\n")
+                    brickPathLen = len(brickPath)+1
+                    if brickPath in path:
+                        actualPath = path[brickPathLen:]
+                        bfd.write (actualPath + "\n")
 
 
-def collectGfid (cl_path, dest_fd):
-    for line in open(cl_path):
+def collectGfid (clPath, destFd):
+    '''
+    Collectes gfids from history changelogs.
+
+    @clPath: Clangelog .processing path.
+    @destFd: File object of gfid_list file.
+
+    Returns: None
+    '''
+    for line in open(clPath):
         details = line.split()
-        #print details[1]
-        dest_fd.write(details[1]+"\n")
+        destFd.write(details[1]+"\n")
+
+    # ajha: close file
 
 def sortUnique(fileName):
-    # can use uniq utility too
-    p = subprocess.Popen(["sort", "-u", "-o",fileName, fileName], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    #print out, err
+    '''
+    Sorts the fileName with unique entries.
 
-def get_changes(brick, scratch_dir, log_file, log_level, start, end):
+    @fileName: File to sort uniquely.
+
+    Returns: None
+
+    '''
+    p = subprocess.Popen(["sort", "-u", "-o",fileName, fileName],\
+                         stdout=subprocess.PIPE)
+    out, err = p.communicate()
+
+def clean (dirPath):
+    '''
+    Recursively unlinks everything *inside* @dirPath, but not the
+    @dirPath itself.
+
+    @dirPath: Directory to clean.
+
+    Returns: None
+    '''
+    for root, dirs, files in os.walk (dirPath):
+        for f in files:
+            os.remove (os.path.join (root, f))
+        for d in dirs:
+            shutil.rmtree (os.path.join (root, d))
+    pass
+
+def getChanges(brick, scratch_dir, logFile, logLevel, start, end):
+    '''
+    Makes use of libgfchangelog's history API to get changelogs containing changes from
+    start and end time. Further collects the modified gfids from the changelogs
+    and writes the list of gfid to 'gfid_list' file.
+
+    @brick          : absolute path of brick location.
+    @scratch_dir    : Scratch directory for history API.
+                      Its @scratchDir/<brickPath-hash>.
+    @logFile        : log file for libgfchangelog.
+    @logLevel       : logging infra...
+    @start          : Start time for history API.
+    @end            : End time for history API.
+
+    Returns: None
+
+    '''
     change_list = []
     try:
-        cl.cl_register(brick, scratch_dir, log_file, log_level)
-        # backup/backup.gfid backup/backup.path
-	print "register passed..."
+        cl.cl_register(brick, scratch_dir, logFile, logLevel)
+
         backupPath = os.path.join (scratch_dir, "backup")
         os.mkdir (backupPath)
+
         # handle file exists
         gfidListPath = os.path.join(scratch_dir,"backup/gfid_list")
         gfidListPathFd  = open(gfidListPath, 'a+')
@@ -66,16 +130,15 @@ def get_changes(brick, scratch_dir, log_file, log_level, start, end):
         cl_path = os.path.join (brick, ".glusterfs","changelogs")
         cl.cl_history_changelog (cl_path, start, end, 3)
 	print "history changelog passed..."
-        
+
         cl.cl_history_scan()
         change_list = cl.cl_history_getchanges()
 	print "scan and gethistory passed..."
+
         if change_list:
             print change_list
+
         for change in change_list:
-            # parse and keep writing on the new file
-            # parse data fops using csnap parser
-            # write discrete parser for entry fops
             collectGfid (change, gfidListPathFd)
             print('done with %s' % (change))
             cl.cl_history_done(change)
@@ -87,21 +150,35 @@ def get_changes(brick, scratch_dir, log_file, log_level, start, end):
     except OSError:
         ex = sys.exc_info()[1]
         print ex
-    #finally: close gfid_list
+    # finally: close gfid_list
 
-def runGetChanges (brick, scratch_dir, log_level, start, end):
+def runGetChanges (brick, scratchDir, logLevel, start, end):
+    '''
+    Wraper over getChanges to minimise arguments from backup host.
+    Modifies scratch_dir for history api by appending <brickpath-hash>
+    to the scratchDir, to avoid collision in case of multiple distribute
+    bricks in same node.
+    Sets log file at @scratchDir/<brickPath-hash>/backup.log
+
+    Return: None
+
+    '''
     brickHash = hashlib.sha1(brick)
-    hashDir = os.path.join(scratch_dir, str(brickHash.hexdigest()))
-
+    hashDir = os.path.join(scratchDir, str(brickHash.hexdigest()))
     # handle OSError
-    os.mkdir (hashDir)
-    log_file = os.path.join(hashDir, "backup.log")
-    get_changes (brick, hashDir, log_file, log_level, start, end)
+    os.makedirs (hashDir)
+
+    logFile = os.path.join(hashDir, "backup.log")
+    getChanges (brick, hashDir, logFile, logLevel, start, end)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("usage: %s <brick> <scratch-dir> <start> <end>"
+    if len(sys.argv) == 5:
+        print "running getChanges..."
+        runGetChanges(sys.argv[1], sys.argv[2], 9, \
+            int(sys.argv[3]), int(sys.argv[4]))
+    elif len (sys.argv) == 2:
+        clean (str(sys.argv[1]))
+    else:
+        print("usage: %s [<brick> <scratch-dir> <start> <end>] | [cleanPath]"
               % (sys.argv[0]))
         sys.exit(1)
-    runGetChanges(sys.argv[1], sys.argv[2], 9, \
-            int(sys.argv[3]), int(sys.argv[4]))
